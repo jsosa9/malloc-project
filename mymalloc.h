ab44de941bdd343111284ac911fcdb0865dec03d
Jose Sosa <js4069@java.cs.rutgers.edu>
2025-09-21 01:48:09 -0400

    initial files

---
+++ mymalloc.h
@@ -0,0 +1,12 @@
+// i dont think we touch this file 
+#ifndef _MYMALLOC_H
+#define _MYMALLOC_H
+
+#define malloc(X) mymalloc(X, __FILE__, __LINE__)
+#define free(X) myfree(X, __FILE__, __LINE__)
+
+void * mymalloc(size_t size, char *file, int line);
+void   myfree(void *ptr, char *file, int line);
+
+
+#endif
